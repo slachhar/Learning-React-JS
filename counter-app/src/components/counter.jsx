049469580585f4fdfa3139044e061bd736b1d43d
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-27 16:04:53 +0530

    setting image attribute

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
     return (
       <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
-  }
+  formatCount() {}
 }
 
 export default Counter;
