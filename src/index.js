a7522f4dea36917bc208e9a7fb0b05ee17325317
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-27 13:55:28 +0530

    hello world in react js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
 
-console.log(element);
+//console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
