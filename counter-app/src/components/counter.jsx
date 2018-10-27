21b33a375915b72ea98aa1d653e7d0f09cc56b88
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-27 16:00:50 +0530

    embedding function

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
   };
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
