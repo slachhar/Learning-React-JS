a03403397d9e3e1956737d890e4d315fc2e264b1
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-30 01:05:51 +0530

    composing components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -30,11 +30,11 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <ul>
+        {/* <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     );
   }
