e4b7faae0442248c69b6a57ca55c8313143d5f5f
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-30 00:49:18 +0530

    passign event arguments

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,19 +11,21 @@ class Counter extends Component {
   //this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
-    //console.log("Increment Clicked", this);
-    // obj.method();
-    // function();
   };
+
+  // doHandlerIncrement = () => {
+  // this.handleIncrement({ id: 1 });
+  //};
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
