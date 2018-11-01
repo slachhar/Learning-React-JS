6e6745092f767d0698fe29f41d6a6eaf5606be76
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-11-01 16:25:55 +0530

    Destructuring Arguments

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,18 +4,16 @@ import Counter from "./counter";
 class Counters extends Component {
   //map method is used in place of foreach loop
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             key={counter.id}
             counter={counter}
           />
--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
 //Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounter }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
-        <span className="badge badge-pill secondary">{props.totalCounter}</span>
+        <span className="badge badge-pill secondary">{totalCounter}</span>
       </a>
     </nav>
   );
